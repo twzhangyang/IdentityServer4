098aee34797961cb2ebbfa4259f2c6744bddd752
ZhangYang <yrzhang@thoughtworks.com>
2019-07-20 21:08:57 +0800

    add AmazonDynamoDb support

--- src/Samples/IdentityServer.Samples.ResourceOwnerClient.Server/Startup.cs
+++ src/Samples/IdentityServer.Samples.ResourceOwnerClient.Server/Startup.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using IdentityServer4.AmazonDynamoDB.Storage.Configuration;
+using IdentityServer4.AmazonDynamoDB.Storage.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityServer.Samples.ResourceOwnerClient.Server
@@ -13,34 +17,53 @@ namespace IdentityServer.Samples.ResourceOwnerClient.Server
     public class Startup
     {
         public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
 
-        public Startup(IHostingEnvironment environment)
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
         {
             Environment = environment;
+            Configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DynamoDBOptions>(Configuration.GetSection("DynamoDB"));
+            services.AddConfigurationDbContext(Configuration,
+                options =>
+                {
+                    var section = Configuration.GetSection("DynamoDB");
+                    section.Bind(options);
+                });
+            
             // uncomment, if you wan to add an MVC-based UI
             //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 
-            const string connectionString = @"Server=tcp:127.0.0.1,1433;Database=IdentityServer;User Id=sa;Password=RestAirline123";
+            const string connectionString =
+                @"Server=tcp:127.0.0.1,1433;Database=IdentityServer;User Id=sa;Password=RestAirline123";
+
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-            
+
             var builder = services.AddIdentityServer()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = x =>
+                        x.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
 //                .AddOperationalStore(options =>
-//                    {
-//                        options.ConfigureDbContext = builder =>
-//                            builder.UseSqlServer(connectionString,
-//                                sql => sql.MigrationsAssembly(migrationsAssembly));
+//                {
+//                    options.ConfigureDbContext = x =>
+//                        x.UseSqlServer(connectionString,
+//                            sql => sql.MigrationsAssembly(migrationsAssembly));
 //
-//                        // this enables automatic token cleanup. this is optional.
-//                        options.EnableTokenCleanup = true;
-//                        options.TokenCleanupInterval = 30; // interval in seconds
-//                    })
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApis())
-                .AddInMemoryClients(Config.GetClients())
+//                    // this enables automatic token cleanup. this is optional.
+////                        options.EnableTokenCleanup = true;
+////                        options.TokenCleanupInterval = 30; // interval in seconds
+//                })
+                
+//                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+//                .AddInMemoryApiResources(Config.GetApis())
+//                .AddInMemoryClients(Config.GetClients())
                 .AddTestUsers(Config.GetUsers());
 
             if (Environment.IsDevelopment())
@@ -55,6 +78,8 @@ namespace IdentityServer.Samples.ResourceOwnerClient.Server
 
         public void Configure(IApplicationBuilder app)
         {
+//            DatabaseMigration.InitializeDatabase(app);
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ src/Samples/IdentityServer4.AmazonDynamoDB.Storage/Configuation/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+using System;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using IdentityServer4.AmazonDynamoDB.Storage.Migration;
+using IdentityServer4.AmazonDynamoDB.Storage.Options;
+using IdentityServer4.AmazonDynamoDB.Storage.Stores;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer4.AmazonDynamoDB.Storage.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfigurationDbContext(this IServiceCollection services,
+            IConfiguration configuration,
+            Action<DynamoDBOptions> actionConfigure = null)
+        {
+            var dynamoDBOptions = new DynamoDBOptions();
+            actionConfigure?.Invoke(dynamoDBOptions);
+
+            var awsOptions = configuration.GetAWSOptions();
+            awsOptions.DefaultClientConfig.ServiceURL = dynamoDBOptions.ServiceURL;
+
+            services.AddDefaultAWSOptions(awsOptions);
+            services.AddAWSService<IAmazonDynamoDB>(awsOptions);
+            services.AddSingleton(x => new DynamoDBContextConfig {TableNamePrefix = dynamoDBOptions.TablePrefix});
+            services.AddTransient(x =>
+            {
+                var client = x.GetService<IAmazonDynamoDB>();
+                var config = x.GetService<DynamoDBContextConfig>();
+                return new DynamoDBContext(client, config);
+            });
+            services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+            services.AddTransient<PersistedGrantTableBuilder>();
+
+            return services;
+        }
+    }
+}
